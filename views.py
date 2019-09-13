961a2b9b2b3f3d3fac6c800ae181ad3c26f98a1f
wkotaro <www.kotaro.wada@gmail.com>
2019-09-14 02:42:02 +0900

    Fix broken flask settings

--- views.py
+++ views.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
-from flask import Flask
-app = Flask(__name__)
+import flask
+
+
+app = flask.Flask(__name__)
+
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='home')
+    return flask.render_template('index.html', title='home')
 
 
 ## おまじない
